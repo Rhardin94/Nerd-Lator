13cb30e41cd067c68a8ed2171e2fab66b93516d5
Robert H <robertjameshardin@aol.com>
2019-03-11 21:36:49 -0500

    ajax request working

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 //On-click that copies translated text to the clipboard
 //Reference found here: "https://codepen.io/shaikmaqsood/pen/XmydxJ"
-$("#copy-to-clipboard").on("click", function() {
+$("#copy-to-clipboard").on("click", function () {
     let transText = $("#translated").val().trim();
     let tempInput = $("<input>");
     $("body").append(tempInput);
@@ -8,3 +8,16 @@ $("#copy-to-clipboard").on("click", function() {
     document.execCommand("copy");
     tempInput.remove();
 });
+//on-click for ajax request
+$("#button").on("click", function () {
+    let textInput = $("#text").val().trim();
+    var queryURL = "https://api.funtranslations.com/translate/klingon.json?text=" + textInput;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        let textResponse = response.contents.translated;
+        console.log(textResponse);
+    })
+})
