5c864b67dc706425db14657aa3a6faf89dd22a2e
mtgalla <mtgalla@gmail.com>
2019-03-18 18:10:56 -0500

    js fix

--- javascript/app.js
+++ javascript/app.js
@@ -47,7 +47,7 @@ $("#cleared").on("click", function() {
     $("#Klingon").empty();
 })
 
-
+})
 //Array for Old English facts (currently with some placeholder data)
 var oeFacts = [
 "Old English language, also called Anglo-saxon, language spoken and written in England before 1100; it is the ancestor of Middle English and Modern English.",
@@ -95,4 +95,4 @@ function facts(){
     },15000);
 
     setTimeout(facts,18000);
-    }
+}
